124323bf3c1fa8c2864c52641e12cf9368f9d335
SafaaMagdyAwad <engsafaamagdy1024@gmail.com>
2024-09-10 16:42:26 +0300

    input from terminal,lists,loops

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,32 +1,79 @@
+import 'dart:io';
 
 
 void main(List<String> arguments) {
-  var x = 20;
-  var y = 5.6;
-  const toKM=1.7; //costant value in compile time عارفه قيمته طول الوقت
-  final now=DateTime.now(); //constant in runtime مش هاعرف قيمته غير لما اشغل البرنامج
-  // var result ="addition value is $(x+y)";
-  print(x+y);
-  print(x-y);
-  print(x*y*toKM);
-  print(x/y);
-  print(x%y);
-  //known values in runtime
-  print(now);
-  print('\'string\'');
-  print("\"string\"");
-  var name='safaa';
-  var message=" wecome  $name";
-  print(message);
-  print(message[3]);
-  var multiline='''
-  safaa
-  magdy
-  awad
-  '''; 
-  print(multiline);
-  String dolars='100';
-  const dolarConvertionOpreator=50;
-  var egyptionBound=dolarConvertionOpreator * int.parse(dolars);
-  print(egyptionBound);
+
+  var list=[1,2,3,4,5];
+  list.add(6);
+  list.add(7);
+  list.insert(2, 44);
+  list.addAll([6,5,4,3,2]);
+  list.insertAll(3, [2,77,55,44,23]);
+  list.removeAt(2);
+  list.removeRange(2, 4);
+  print(list);
+
+
+// atm programme
+  int ammount=1000;
+  bool repeat=true;
+  do {
+    String message;
+    print ("1.Check palance");
+    print ("2.withdraw cach");
+    print ("3. deposite cach");
+    print ("Enter your choise");
+    int choice=int.parse(stdin.readLineSync()!);
+    switch (choice) {
+      case 1:
+        message="your palance is $ammount";
+        break;
+      case 2:
+        message="add cach to the machine";
+        break;
+      case 3:
+        print("how much would you like to take?");
+        int mony=int.parse(stdin.readLineSync()!);
+        if(mony>ammount){
+          message='invalid';
+        }else{
+          ammount=ammount+mony;
+          message='done!';
+        }
+        break;
+      default:message="invalid";
+    }
+        print(message);
+  } while (repeat);
+
+
+
+  // print("enter weight");
+  // var weight=double.parse(stdin.readLineSync()!);
+  // print("enter height in cm");
+  // var height=double.parse(stdin.readLineSync()!);
+  // var bmi;
+  // var result;
+  // bmi=weight*10000/(height*height);
+  // print(bmi);
+  // if( bmi>=25 && bmi<30){
+  //   result="OverWeight";
+  // }else if(bmi>=20 && bmi<25){
+  //   result="perfect";
+  // }else{
+  //   result="oh what is that yor in danger ??";
+  // }
+  // print("bmi is");
+  // print(result);
+
+  // name=name!.toLowerCase();
+
+  // int x= 0;
+  // do {
+  //   if (x%2 != 0) {
+  //     print(x);
+  //   }
+  //   x++;
+  // } while (x<=10);
+  
 }
